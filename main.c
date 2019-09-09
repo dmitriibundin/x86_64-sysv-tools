877e0879dc371f4872deba2493e0d390bad5a037
Dmitrii Bundin <dmitrii.bundin.a@gmail.com>
2019-09-09 16:27:42 +0300

    cpuid: cache size, number of cores, vendor string

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdalign.h>
+#include "min.h"
+
+int main(void){
+    unsigned int L1_cache_size = get_cache_line_size(1);
+    unsigned int L2_cache_size = get_cache_line_size(2);
+    unsigned int L3_cache_size = get_cache_line_size(3);
+    printf("L1 size = %u, L2 size = %u, L3 size = %u\n", L1_cache_size, L2_cache_size, L3_cache_size);
+
+    test(31);
+
+    unsigned int logical_cores_count_1 = cpu_topology_get_logical_cores();
+    unsigned int logical_cores_count_2 = cpu_topology_get_logical_cores();
+    unsigned int logical_cores_count_3 = cpu_topology_get_logical_cores();
+    printf("Logical cores 1 = %u\n", logical_cores_count_1);
+    printf("Logical cores 2 = %u\n", logical_cores_count_2);
+    printf("Logical cores 3 = %u\n", logical_cores_count_3);
+    _Alignas(AVX2_ALIGNEMENT) long long longs[] = {1333, 3123, 3, 122};
+    long long *min_ptr = avx2_find_min(longs);
+    printf("The minimum is %lli\n", *min_ptr);
+}
---
+++ min.h
@@ -0,0 +1,15 @@
+#ifndef AVX2_MIN_H
+#define AVX2_MIN_H
+
+#define AVX2_ALIGNEMENT 32
+
+long long * avx2_find_min(long long *);
+
+unsigned int get_cache_line_size(unsigned int cache_level);
+
+unsigned int cpu_topology_get_logical_cores(void);
+
+void test(int a){
+    (void) a;
+}
+#endif //AVX2_MIN_H
