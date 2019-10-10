c88f6681ff651543050e2b84428d49a26752ddfb
Dmitrii Bundin <dmitrii.bundin.a@gmail.com>
2019-10-10 09:36:56 +0300

    asm inc filenames are generated within cmake

--- generate_source.py
+++ generate_source.py
@@ -2,6 +2,7 @@
 from dataclasses import dataclass
 import re
 import sys
+import argparse
 from pathlib import Path
 
 #Convert C header files to nasm
@@ -53,18 +54,22 @@ def object_defines(header_file):
     return [directive for directive in [parse_directive_line(raw_directive) for raw_directive in raw_directives] if directive]
 
 if __name__ == "__main__":
-    args_count = len(sys.argv)
-    if(args_count != 3):
-        print (f"Illegal number of command line arguments. Expected: 2, actual: {args_count - 1}")
-        sys.exit(1)
-    c_header_path = sys.argv[1]
-    nasm_output_dir = sys.argv[2]
-    with open(c_header_path) as fd:
-        c_header = fd.read()
+    parser = argparse.ArgumentParser(description='Parse generator arguments')
+    parser.add_argument("--c-header-nasm-inc-pairs", type=lambda str: [pair.split(':') for pair in str.split(',')],
+            help="Comma-separated list of colon-separated pairs of source-c-header:destination-generated-nasm-inc, \
+                    e.g. /path/file1.c:/generated/path/file2.inc,/another_path/file3.h:/another/generated/file4.inc, \
+                    where all filenames and extensions are not restricted to those ende with 'h', 'c', or 'inc'.")
+    args = parser.parse_args()
+    
+    if(len(sum(arg
+    print(args.c_header_nasm_inc_pairs)
 
-    nasm_define_directives = [F"%define {object_define.macro_name} {object_define.macro_value or ''}" for object_define in object_defines(c_header)]
+    print(flattened_dict)
+    if(len(flattened_dict) > len(set(flattened_dict))):
+        raise Exception("Non unique element encountered")
 
-    nasm_header_path = Path(nasm_output_dir, Path(c_header_path).stem).with_suffix(NASM_INCLUDE_SUFFIX)
-
-    with open(nasm_header_path, 'w+') as nasm_header_fd:
-        nasm_header_fd.writelines(line + '\n' for line in nasm_define_directives)
+    #TODO: Validate the dictionary that it the c_header_path,nasm_header_path are all distinct
+    for c_header_path, nasm_header_path in args.c_header_nasm_inc_pair:
+        nasm_define_directives = [F"%define {object_define.macro_name} {object_define.macro_value or ''}" for object_define in object_defines(c_header_path)]
+        with open(nasm_header_path, 'w+') as nasm_header_fd:
+            nasm_header_fd.writelines(line + '\n' for line in nasm_define_directives)
