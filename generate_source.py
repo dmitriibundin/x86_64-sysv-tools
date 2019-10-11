a2cd8e4b06cc40acc1004b43d4dc868e1b4ff4cf
Dmitrii Bundin <dmitrii.bundin.a@gmail.com>
2019-10-11 07:36:15 +0300

    header converter fixed

--- generate_source.py
+++ generate_source.py
@@ -55,21 +55,19 @@ def object_defines(header_file):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parse generator arguments')
-    parser.add_argument("--c-header-nasm-inc-pairs", type=lambda str: [pair.split(':') for pair in str.split(',')],
+    parser.add_argument("--c-header-nasm-inc-pairs", type=lambda str: [pair.split(':', 1) for pair in str.split(',')],
             help="Comma-separated list of colon-separated pairs of source-c-header:destination-generated-nasm-inc, \
                     e.g. /path/file1.c:/generated/path/file2.inc,/another_path/file3.h:/another/generated/file4.inc, \
                     where all filenames and extensions are not restricted to those ende with 'h', 'c', or 'inc'.")
     args = parser.parse_args()
-    
-    if(len(sum(arg
-    print(args.c_header_nasm_inc_pairs)
-
-    print(flattened_dict)
+    flattened_dict = sum(args.c_header_nasm_inc_pairs, [])
     if(len(flattened_dict) > len(set(flattened_dict))):
         raise Exception("Non unique element encountered")
 
     #TODO: Validate the dictionary that it the c_header_path,nasm_header_path are all distinct
-    for c_header_path, nasm_header_path in args.c_header_nasm_inc_pair:
-        nasm_define_directives = [F"%define {object_define.macro_name} {object_define.macro_value or ''}" for object_define in object_defines(c_header_path)]
+    for c_header_path, nasm_header_path in args.c_header_nasm_inc_pairs:
+        with open(c_header_path) as c_header_path_fd:
+            c_header = c_header_path_fd.read()
+        nasm_define_directives = [F"%define {object_define.macro_name} {object_define.macro_value or ''}" for object_define in object_defines(c_header)]
         with open(nasm_header_path, 'w+') as nasm_header_fd:
             nasm_header_fd.writelines(line + '\n' for line in nasm_define_directives)
